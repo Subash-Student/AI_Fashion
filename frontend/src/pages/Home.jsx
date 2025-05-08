24131fcd15199462f54baf3873ff9d8c7d493921
Subash <subashmurugan2021@gmail.com>
2025-05-08 15:20:17 +0530

    fix carousel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import Hero from '../components/Hero'
+import Carousel from "../components/Carousel"
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
@@ -10,9 +11,23 @@ import Women from '../components/Women'
 import Kids from '../components/Kids'
 import { ShopContext } from '../context/ShopContext'
 
+
 const Home = () => {
   const {token} = useContext(ShopContext);
-
+  const slides =  [
+    {
+        "scr": "https://picsum.photos/seed/picsum/600/400",
+        "alt": "image 1"
+    },
+    {
+        "scr": "https://picsum.photos/id/77/600/400",
+        "alt": "image 2"
+    },
+    {
+        "scr": "https://picsum.photos/id/8/600/400",
+        "alt": "image 3"
+    }
+]
   return (
     <div>
       <div className='Caro'>
